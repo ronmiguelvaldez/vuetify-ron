61d61391a86bbf4b22a11a42b4c0b049808b3774
rmvaldez14 <116440999+ronmiguelvaldez@users.noreply.github.com>
2023-08-23 18:55:23 +0800

    Update Todo.vue

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -3,14 +3,13 @@
     <h1> Todo Page</h1>
     <v-form>
       <v-autocomplete
-      label="Todo List"
-      :items="['Enroll Biometrics','Get BDO Card', 'Learn Code Igniter', 'Study Vue JS', 'Study MySQL',]"
+      label="Add a Task"
+      :items="tasks"
       v:model="input"
+      outlined
       >
     </v-autocomplete>
     <v-btn color="primary" class="mr-1" @click="addList">Add List</v-btn>
-    <v-list lines="one">
-    </v-list>
   </v-form>
       
   </div>
@@ -24,7 +23,7 @@
     },
     data() {
       return {
-        
+        tasks: ['Enroll Biometrics', 'Get BDO Card', 'Learn Code Igniter', 'Study Vue JS', 'Study MySQL']
       }
     },
     methods: {
