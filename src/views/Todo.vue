0d0b7daa6533ee4ebe685ab13992ee6e92198afe
rmvaldez14 <116440999+ronmiguelvaldez@users.noreply.github.com>
2023-08-23 15:21:50 +0800

    first commit

---
+++ src/views/Todo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todo pa-6 ma-10">
+    <h1> Todo Page</h1>
+    <v-autocomplete
+    label="Todo List"
+    :items="['Enroll Biometrics','Get BDO Card', 'Learn Code Igniter', 'Study Vue JS', 'Study MySQL']"
+    v:model="input"
+    >
+    </v-autocomplete>
+    <v-btn color="primary" class="mr-1" @click="addList">Add List</v-btn>
+    <v-list lines="one">
+    </v-list>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+
+    components: {
+
+    },
+    data() {
+      return {
+        habits: {
+          lifestyle: 'Workout',
+          food: 'Eating'
+        }
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
